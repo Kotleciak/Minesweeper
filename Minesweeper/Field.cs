becda6061c9ea013d09ca89b098b0254a9259602
Leon Zaczek <zaczek.leon08@gmail.com>
2025-07-03 22:42:30 +0200

    Dodaj pliki projektów.

---
+++ Minesweeper/Field.cs
@@ -0,0 +1,19 @@
+﻿namespace Minesweeper
+{
+    public class Field
+    {
+        public string ID { get; set; }
+        public int Xindex { get; set; }
+        public int Yindex { get; set; }
+        public bool IsMine { get; set; }
+        public int AdjacentMines { get; set; }
+        public enum FieldState
+        {
+            Hidden,
+            Revealed,
+            Flagged,
+        }
+        public FieldState State { get; set; }
+
+    }
+}
